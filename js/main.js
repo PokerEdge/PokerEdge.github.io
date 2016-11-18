e22d82bbe5ed3f4558aed4cce3d638181fc58b4a
Reid Young <reid@pokersprout.com>
2016-11-18 18:15:18 -0500

    Several JavaScript changes to add more interactivity to the form elements
    Management of conflicting checkbox input elements so that certain
    checkboxes are disabled as others become selected.
    
    Disabled checkbox text color is styled.
    
    Checkboxes are not disabled when conflicting checkbox is deselected by
    the user, e.g. user clicks conflicting box and then clicks it again to
    mark the box not checked.
    
    totalCost is counting the amount of money to be charged to the user
    upon successful check out based on interaction with a (set of) checkbox
    element(s).

--- js/main.js
+++ js/main.js
@@ -1,6 +1,17 @@
+//Append total cost below list of activities
+  //Hide if cost is $0
+  //Append THIS HTML IN ORDER TO HIDE/SHOW IT AS IT UPDATES ON CHANGE OF CHECKBOX
+  // $('.activities').append('<span id="totalCost">$' + totalCost + '</span>')
+
+
+//Hide conflicting data error message and styling on page load
+
 
 //Initially hide the text input that should only show if user selects "Other" from the "Job Role" dropdown menu
 $('#other-title').hide();
+
+//Hides all color selections until a Design element is chosen from the Deisgn drop down menu
+$('select#color').children(":nth-child(n+1)").hide();
 
 //Apply handler to dropdown menu to take actions when the values change
 $('select#title').change(function(){
@@ -9,29 +20,29 @@ $('select#title').change(function(){
 
 });
 
+//Apply handler to....
 $('select#design').change(function(){
 
   checkDesignValue();
 
 });
 
+//Apply handler to....
 $('input:checkbox').change(function(){ //Event handler isn't correct because checkbox value is always the same
 
   checkCheckBox();
 
 });
 
+//Function checks to see which 'Job Role' value is selected, then shows or hides a text field based on the selected value
 function checkJobRoleValue(){
 
-  //Function is to check to see which value is selected
   if($('select#title').val() === 'other'){
 
-    //Show text input field if "other" option is selected
     $('#other-title').show();
 
   } else{
 
-    //Hide text input field if non-"other" option is selected
     $('#other-title').hide();
 
   }
@@ -40,48 +51,139 @@ function checkJobRoleValue(){
 
 function checkDesignValue(){
 
-  //Check if selected design value is "js puns" or "heart js"
-  if($('select#design').val() === 'js puns'){
+  $('select#color').children().show();
+
+
+  if($('select#design').val() === 'js puns'){ //Check if selected design value is "js puns" or "heart js"
 
     //If design value is "js puns", color menu should display options "Cornflower Blue", "Dark Slate Grey" and "Gold" (top 3 elements in list)
-    $('select#color').children().show();
-    $('select#color').children(":nth-child(-n+4)").hide();
+
+    // $('select#color').children(":nth-child(n+1)").hide(); //Hide first child
+    $('select#color').children(":nth-child(n+5)").hide();
 
 
   } else if ($('select#design').val() === 'heart js') {
 
     //Else if design value is "heart js", color menu should display options "Tomato", "Steel Blue" and "Dim Grey" (bottom 3 elements in list)
-    $('select#color').children().show();
-    $('select#color').children(":nth-child(n+5)").hide();
+    // $('select#color').children(":nth-child(n+1)").hide(); //Hide first child
+    $('select#color').children(":nth-child(-n+4)").hide();
 
-  }  else {
+  } else {
 
-    //Show all choices in dropdown menu
-    $('select#color').children().show();
+    //Else if design value is not one of the choices, hide options except "<-- Please select T-shirt theme"
+    $('select#color').children(":nth-child(n+1)").hide(); 
 
   }
 
 }
 
-
+//Function checks for and disables conflicting events depending on checkboxes checked by the user and
+//  maintains a total of the amount of money to be charged to the user given user input on any (set of) checkbox element(s) and displays the total
 function checkCheckBox(){
 
   // "Register for Activities" section of the form:
-  //   Some events are at the same time as others. If the user selects a workshop, don't allow selection 
-  //     of a workshop at the same date and time -- you should disable the checkbox and visually indicate that 
-  //     the workshop in the competing time slot isn't available.
-  //   When a user unchecks an activity, make sure that competing activities (if there are any) are no longer 
-  //     disabled.
+
   //   As a user selects activities to register for, a running total is listed below the list of checkboxes. 
   //     For example, if the user selects "Main conference" then Total: $200 should appear. If they add 1 workshop, 
   //     the total should change to Total: $300.
-      // Gecko and IE will ignore the value specified in the HTML source, if the value was changed before the reload
+
+  
+  var totalCost = 0;
+
+  if ($('#all').is(":checked")){
+
+    totalCost += 200;
+
+  } else {
+
+    totalCost - 200;
+
+  }
+
+  if ($('#js-frameworks').is(":checked")){
+
+    $('#express').prop('disabled', true);
+    $('#express').parent().css("color", "#7c8f9b");
+    totalCost += 100;
+
+  } else {
+
+    $('#express').prop('disabled', false);
+    $('#express').parent().css("color", "#000");
+    totalCost - 100;
+
+  }
+
+  if ($('#express').is(':checked')){
+
+    $('#js-frameworks').prop('disabled', true);
+    $('#js-frameworks').parent().css("color", "#7c8f9b");
+    totalCost += 100;
+
+  } else {
+
+    $('#js-frameworks').prop('disabled', false);
+    $('#js-frameworks').parent().css("color", "#000");
+    totalCost - 100;
+
+  }
+
+  if ($('#js-libs').is(":checked")) {
+
+    $('#node').prop('disabled', true);
+    $('#node').parent().css("color", "#7c8f9b");
+    totalCost += 100;
+
+  } else {
+
+    $('#node').prop('disabled', false);
+    $('#node').parent().css("color", "#000");
+    totalCost - 100;
+
+  }
+
+  if ($('#node').is(":checked")){
+
+    $('#js-libs').prop('disabled', true);
+    $('#js-libs').parent().css("color", "#7c8f9b");
+    totalCost += 100;
+
+  } else {
+
+    $('#js-libs').prop('disabled', false);
+    $('#js-libs').parent().css("color", "#000");
+    totalCost - 100;
+
+  }
+
+  if ($('#build-tools').is(":checked")){
+
+  totalCost += 100;
+
+  } else {
+
+    totalCost - 100;
+  }
+
+  if ($('#npm').is(":checked")){
+
+    totalCost += 100;
+
+  } else {
+
+    totalCost - 100;
+  }
+
+//Hide the total cost message as a reset before a new total cost is calculuate onChange of checkbox value
+  //possibly manipulate the innerHTML or text() of the styled span element?
 
 
-  // Get the value from a checked checkbox
-  console.log($('input:checkbox:checked').val());
+// $('.activities span').hide();
+// $('.activities span').show();
 
-
+  // $('.activities').remove('<span id="totalCost">$' + totalCost + '</span>');
+  $('.activities').append('<span id="totalCost">$' + totalCost + '</span>'); //Should simply show the message here (append globally?)
+  console.log("$" + totalCost);
 
 }
 
@@ -114,6 +216,9 @@ the design selected in the "Design" menu.
     "Dark Slate Grey," and "Gold."
   If the user selects "Theme - I ♥ JS" then the color menu should only display "Tomato," "Steel Blue," 
     and "Dim Grey."
+
+No color options appear in the “Color” menu until the user chooses a T-Shirt theme. The Color menu reads “Please select a T-shirt theme” 
+  until a theme is selected from the “Design” menu.
 
 "Register for Activities" section of the form:
   Some events are at the same time as others. If the user selects a workshop, don't allow selection 
@@ -166,6 +271,6 @@ Hide the "Color" label and select menu until a T-Shirt design is selected from t
 
 Style the "select" menus (drop down menus) on the form, so they match the styling of the text fields
 
-Validate the credit card number to make sure it's entered in a valid format
+Validate the credit card number to make sure it's entered in a valid format $('#cvv')
 
 */
