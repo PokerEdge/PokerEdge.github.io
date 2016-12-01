549dac80c86351ceb2130fb4d4c4c113c6b5a184
Reid Young <reid@pokersprout.com>
2016-12-01 15:46:06 -0500

    Form validation improvements for several use cases
    Form validation improvements for several use cases: credit card is no
    longer shown after user attempts to submit an invalid non-credit card
    form of payment.
    
    Error messages for Name and for Email are properly styled.

--- js/main.js
+++ js/main.js
@@ -313,17 +313,13 @@ function validateForm(){
   //Hides all color selections until a Design element is chosen from the Deisgn drop down menu
   $('select#color').children(":nth-child(n+1)").hide();
 
-  //Hide all payment option information on page load that is to be displayed after user chooses an option from the Payment Info dropdown menu
-  $('div#credit-card').show();
-  $('div#paypal-option').hide();
-  $('div#bitcoin-option').hide();
-
   //Error counter used to keep track of all invalid form elements within this function
   var errorCount = 0;
 
   var nameInput = $('input#name');   
   if(nameInput.val() === ""){
     
+    $('#nameLabel').css({'color': '#9f3b53', 'font-weight':'500'});
     $('#nameError').show().css({'color': '#9f3b53', 'font-weight':'500'});
     errorCount++;
   }
@@ -335,10 +331,13 @@ function validateForm(){
   //Check that email address is valid and display error message and styling if not valid
   if(!email_regex.test(email_address.val())){ 
         
+    $('#emailLabel').css({'color': '#9f3b53', 'font-weight':'500'});
     $('#emailError').show().css({'color': '#9f3b53', 'font-weight':'500'});
     errorCount++;
   }  
 
+
+//SELECTOR FOR T-SHIRT ERROR IS INCORRECT************
 
   //Check that a t-shirt color has been chosen else display below error styles and message
   if (!$('#color option:selected').length){
@@ -400,26 +399,21 @@ function validateForm(){
 
 
 /*
-
 ********** INTERACTIVE FORM PROJECT REQUIREMENTS **********
-
 **Create and link a JavaScript file to index.html
   Create a JavaScript file inside the "js" folder and link it to index.html
   If you're using jQuery, link index.html to the latest version of jQuery
-
 **"Job Role" section of the form: reveal a text field when the "Other" option 
 is selected from the "Job Role" drop down menu
   Make sure you add an text input field
   Use the id of "other-title" for the field
   Add placeholder text of "Your Title" for the field
-
 **"T-Shirt Info" section of the form: for the T-Shirt color menu, only display the options that match 
 the design selected in the "Design" menu.
   If the user selects "Theme - JS Puns" then the color menu should only display "Cornflower Blue," 
     "Dark Slate Grey," and "Gold."
   If the user selects "Theme - I ♥ JS" then the color menu should only display "Tomato," "Steel Blue," 
     and "Dim Grey."
-
 **"Register for Activities" section of the form:
   Some events are at the same time as others. If the user selects a workshop, don't allow selection 
     of a workshop at the same date and time -- you should disable the checkbox and visually indicate that 
@@ -429,7 +423,6 @@ the design selected in the "Design" menu.
   As a user selects activities to register for, a running total is listed below the list of checkboxes. 
     For example, if the user selects "Main conference" then Total: $200 should appear. If they add 1 workshop, 
     the total should change to Total: $300.
-
 **Payment Info section of the form: display payment sections based on chosen payment option
   The "Credit Card" payment option should be selected by default and result in the display of the #credit-card 
     div, and hide the "Paypal" and "Bitcoin information.
@@ -437,10 +430,8 @@ the design selected in the "Design" menu.
     information and the "Bitcoin" information.
   When a user selects the "Bitcoin" payment option, display the Bitcoin information, and hide the credit card 
     information. 
-
 **Set focus on the first text field:
   When the page loads, give focus to the first text field
-
 Form validation: display error messages and don't let the user submit the form (submit() or reset() register button depending on conditions) if 
   any of these validation errors exist:
     Name field can't be empty
@@ -450,35 +441,24 @@ Form validation: display error messages and don't let the user submit the form (
     At least one activity must be checked from the list under "Register for Actitivities."
     Payment option must be selected.
     If "Credit card" is the selected payment option, make sure the user supplied a credit card number, a zip code, and a 3 number CVV value.
-
 No color options appear in the “Color” menu until the user chooses a T-Shirt theme. The Color menu reads “Please select a T-shirt theme” 
   until a theme is selected from the “Design” menu. (Hide the first option once selection changes! rest is complete)
-
 Make sure your program is free of syntax errors.
   You can monitor any errors by looking at the Developer Tools console in your browser.
   Use jsHint (see Resources links) to check your code for syntax and formatting problems.
-
 Make sure you add code comments to explain how your programming works.
-
 When JavaScript is switched off or unavailable, all information required to be filled out should be visible. 
   For example, the “Your Job Role” text field should already be available if someone selects “Other."
-
 Before you submit your project for review, make sure you can check off all of the items on the Student Project 
   Submission Checklist. The checklist is designed to help you make sure you’ve met the grading requirements 
   and that your project is complete and ready to be submitted!  
-
 */
 
 
 
 /*
-
 ********** EXTRA CREDIT **********
-
 Hide the "Color" label and select menu until a T-Shirt design is selected from the "Design" menu.
-
 Style the "select" menus (drop down menus) on the form, so they match the styling of the text fields
-
 Validate the credit card number to make sure it's entered in a valid format $('#cvv')
-
 */
