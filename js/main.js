a3f2a3e036fc2c943f350cc97c56ec6aa915b2ed
Reid Young <reid@pokersprout.com>
2016-12-02 14:43:21 -0500

    Simplified syntax, improved commenting, tested several use cases
    Simplified syntax, improved commenting, tested several use cases

--- js/main.js
+++ js/main.js
@@ -1,8 +1,3 @@
-//REMOVE CSS FROM MESSAGES BELOW BECAUSE THEY NEED TO BE RE-ADDED FOR WHEN THE ERROR MESSSAGE IS SHOWN
-//CREATE .errorStyle CLASS
-//CREATE .defaultStyle CLASS (not .errorStyle)
-
-
 //Give focus to input#name text input on page load
 $('input#name').focus();
 
@@ -52,8 +47,8 @@ $('#cvvTitle').css({'color': '#000', 'font-weight':'normal'});
 //Initially hide the text input that should only show if user selects "Other" from the "Job Role" dropdown menu
 $('#other-title').hide();
 
-//Hides all color selections until a Design element is chosen from the Deisgn drop down menu
-$('select#color').children(":nth-child(n+1)").hide();
+//Hides all color selections until a Design element is chosen from the Design drop down menu
+// $('select#color').children(":nth-child(n+1)").hide();
 
 //Hide all payment option information on page load that is to be displayed after user chooses an option from the Payment Info dropdown menu
 $('div#credit-card').show();
@@ -76,22 +71,21 @@ $('select#design').change(function(){
 });
 
 //Apply handler to all checkboxes to call checkCheckBox() when the values change
-//***********************
-$('input:checkbox').change(function(){ //Event handler isn't correct because checkbox value is always the same
+$('input:checkbox').change(function(){
 
   checkCheckBox();
 
 });
 
 //Apply handler to payment dropdown to call checkPaymentOption() when the values change
-$('select#payment').change(function(){ //Event handler isn't correct because checkbox value is always the same
+$('select#payment').change(function(){
 
   checkPaymentOption();
 
 });
 
 //Apply click handler on submit button to fire function on click to prevent invalid data form submissions
-$('button[type="submit"]').click(function(e){ //Event handler isn't correct because checkbox value is always the same
+$('button[type="submit"]').click(function(e){
   
   e.preventDefault();
   validateForm();
@@ -117,37 +111,31 @@ function checkJobRoleValue(){
 function checkDesignValue(){
 
 //Shows color selection label and drop down menu if a design drop down element is selected
-if('select#design:selected'){
+if($('select#design').val() !== 'select-design'){
   $('#colors-js-puns').show();
-  $('#select-theme').show();
-}
+  $('select#color').children().show();
 
-$('select#color').children().show();
+    if($('select#design').val() === 'js puns'){
 
+      //If design value is "js puns", color menu should display options "Cornflower Blue", "Dark Slate Grey" and "Gold" (top 3 elements in list)
+      $('select#color').children(":nth-child(n+5)").hide();
+      $('#select-theme').hide();
 
-  if($('select#design').val() === 'js puns'){ //Check if selected design value is "js puns" or "heart js"
+    } else if ($('select#design').val() === 'heart js') {
 
-    //If design value is "js puns", color menu should display options "Cornflower Blue", "Dark Slate Grey" and "Gold" (top 3 elements in list)
+      //Else if design value is "heart js", color menu should display options "Tomato", "Steel Blue" and "Dim Grey" (bottom 3 elements in list)
+      $('select#color').children(':nth-child(-n+4)').hide();
+      
+      if ($('select#color').val() !== 'select-theme'){
+          $('#select-theme').hide();
+      }
+    } 
+    else {
 
-    // $('select#color').children(":nth-child(n+1)").hide(); //Hide first child
-    $('select#color').children(":nth-child(n+5)").hide();
-    $('#select-theme').hide();
-
-
-  } else if ($('select#design').val() === 'heart js') {
-
-    //Else if design value is "heart js", color menu should display options "Tomato", "Steel Blue" and "Dim Grey" (bottom 3 elements in list)
-    // $('select#color').children(":nth-child(n+1)").hide(); //Hide first child
-    $('select#color').children(":nth-child(-n+4)").hide();
-    $('#select-theme').hide();
-
-  } else {
-
-    //Else if design value is not one of the choices, hide options except "<-- Please select T-shirt theme"
-    $('select#color').children(":nth-child(n+1)").hide(); 
-
+      //Else if design value is not one of the choices, hide options except "<-- Please select T-shirt theme"
+      $('select#color').children(':nth-child(n+1)').hide(); 
+    }
   }
-
 }
 
 //Function checks for and disables conflicting events depending on checkboxes checked by the user and
@@ -313,10 +301,7 @@ function validateForm(){
   //Initially hide the text input that should only show if user selects "Other" from the "Job Role" dropdown menu
   $('#other-title').hide();
 
-  //Hides all color selections until a Design element is chosen from the Deisgn drop down menu
-  $('select#color').children(":nth-child(n+1)").hide();
-
-  //Error counter used to keep track of all invalid form elements within this function
+  //Error counter used to count invalid form elements
   var errorCount = 0;
 
   var nameInput = $('input#name');   
@@ -337,13 +322,10 @@ function validateForm(){
     $('#emailLabel').css({'color': '#9f3b53', 'font-weight':'500'});
     $('#emailError').show().css({'color': '#9f3b53', 'font-weight':'500'});
     errorCount++;
-  }  
-
-
-//SELECTOR FOR T-SHIRT ERROR IS INCORRECT************
+  }
 
   //Check that a t-shirt color has been chosen else display below error styles and message
-  if (!$('#color option:selected').length){
+  if ($('select#color').val() === 'select-theme'){
 
     $('#shirtError').show().css({'color': '#9f3b53', 'font-weight':'500', 'font-size':'16px'});
   }
@@ -353,12 +335,7 @@ function validateForm(){
     
     $('#activityError').show();
     errorCount++;
-  }
-
-  //Payment option must be selected (CC is selected by defualt, but can check if payment 
-    //option val === "Select Payment Method")
-    //*************************************************
-    
+  }  
 
   var ccNumber = $('#cc-number');
 
@@ -391,77 +368,8 @@ function validateForm(){
 
   
   if (errorCount === 0){
+
     $('button[type="submit"]').submit();
     console.log("form submitted");
   }
-
-
 }
-
-
-
-
-/*
-********** INTERACTIVE FORM PROJECT REQUIREMENTS **********
-**Create and link a JavaScript file to index.html
-  Create a JavaScript file inside the "js" folder and link it to index.html
-  If you're using jQuery, link index.html to the latest version of jQuery
-**"Job Role" section of the form: reveal a text field when the "Other" option 
-is selected from the "Job Role" drop down menu
-  Make sure you add an text input field
-  Use the id of "other-title" for the field
-  Add placeholder text of "Your Title" for the field
-**"T-Shirt Info" section of the form: for the T-Shirt color menu, only display the options that match 
-the design selected in the "Design" menu.
-  If the user selects "Theme - JS Puns" then the color menu should only display "Cornflower Blue," 
-    "Dark Slate Grey," and "Gold."
-  If the user selects "Theme - I ♥ JS" then the color menu should only display "Tomato," "Steel Blue," 
-    and "Dim Grey."
-**"Register for Activities" section of the form:
-  Some events are at the same time as others. If the user selects a workshop, don't allow selection 
-    of a workshop at the same date and time -- you should disable the checkbox and visually indicate that 
-    the workshop in the competing time slot isn't available.
-  When a user unchecks an activity, make sure that competing activities (if there are any) are no longer 
-    disabled.
-  As a user selects activities to register for, a running total is listed below the list of checkboxes. 
-    For example, if the user selects "Main conference" then Total: $200 should appear. If they add 1 workshop, 
-    the total should change to Total: $300.
-**Payment Info section of the form: display payment sections based on chosen payment option
-  The "Credit Card" payment option should be selected by default and result in the display of the #credit-card 
-    div, and hide the "Paypal" and "Bitcoin information.
-  When a user selects the "PayPal" payment option, display the Paypal information, and hide the credit card 
-    information and the "Bitcoin" information.
-  When a user selects the "Bitcoin" payment option, display the Bitcoin information, and hide the credit card 
-    information. 
-**Set focus on the first text field:
-  When the page loads, give focus to the first text field
-Form validation: display error messages and don't let the user submit the form (submit() or reset() register button depending on conditions) if 
-  any of these validation errors exist:
-    Name field can't be empty
-    Email field must be a validly formatted e-mail address (you don't have to check that it's a real e-mail address, just that it's 
-      formatted like one: dave@teamtreehouse.com for example. You'll need to use a regular expression to get this requirement. See the list
-      of Resources for links to learn about regular expressions.
-    At least one activity must be checked from the list under "Register for Actitivities."
-    Payment option must be selected.
-    If "Credit card" is the selected payment option, make sure the user supplied a credit card number, a zip code, and a 3 number CVV value.
-No color options appear in the “Color” menu until the user chooses a T-Shirt theme. The Color menu reads “Please select a T-shirt theme” 
-  until a theme is selected from the “Design” menu. (Hide the first option once selection changes! rest is complete)
-Make sure your program is free of syntax errors.
-  You can monitor any errors by looking at the Developer Tools console in your browser.
-  Use jsHint (see Resources links) to check your code for syntax and formatting problems.
-Make sure you add code comments to explain how your programming works.
-When JavaScript is switched off or unavailable, all information required to be filled out should be visible. 
-  For example, the “Your Job Role” text field should already be available if someone selects “Other."
-Before you submit your project for review, make sure you can check off all of the items on the Student Project 
-  Submission Checklist. The checklist is designed to help you make sure you’ve met the grading requirements 
-  and that your project is complete and ready to be submitted!  
-*/
-
-
-
-/*
-********** EXTRA CREDIT **********
-Hide the "Color" label and select menu until a T-Shirt design is selected from the "Design" menu.
-Style the "select" menus (drop down menus) on the form, so they match the styling of the text fields
-Validate the credit card number to make sure it's entered in a valid format $('#cvv')
-*/
